f0da83172dea7e1ddcf0de75b3a8d01a19592b08
Alessandro Morgantini <gpz500@technologist.com>
2020-05-01 12:13:49 +0200

    Add caddy (https://caddyserver.com) as reverse proxy for homeassistant

--- home-assistant/get_certificate.sh
+++ home-assistant/get_certificate.sh
@@ -6,4 +6,5 @@ DEST_DIR=/home/homeassistant
 scp "root@monsterino.fritz.box:/etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem" "$DEST_DIR/$DOMAIN_NAME/fullchain.pem"
 scp "root@monsterino.fritz.box:/etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem" "$DEST_DIR/$DOMAIN_NAME/privkey.pem"
 chmod 0644 "$DEST_DIR/$DOMAIN_NAME/fullchain.pem"
-chmod 0600 "$DEST_DIR/$DOMAIN_NAME/privkey.pem"
+chmod 0640 "$DEST_DIR/$DOMAIN_NAME/privkey.pem"
+sudo systemctl reload caddy.service
